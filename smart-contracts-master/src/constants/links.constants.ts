bd70736bdc4a9deb7d8c286a87b5b2823ffcdb64
Daniil Ketov <danielketovone@gmail.com>
2023-05-03 10:50:14 +0500

    Delete smart-contracts-master directory

--- smart-contracts-master/src/constants/links.constants.ts
+++
@@ -1,6 +0,0 @@
-export const LOGO = '/';
-export const HIRE = 'mailto:d@x.la';
-export const LINKED = 'https://www.linkedin.com/in/dktv/';
-export const TG = 'https://t.me/Joichiro';
-export const ARTICLES = 'https://paragraph.xyz/@ketov.eth';
-export const EMAIL = 'd@x.la';
