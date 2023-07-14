ffa1bd0c33666d2e3fc182714371ced8fd64019b
Daniil Ketov <danielketovone@gmail.com>
2023-07-14 10:48:52 +0000

    Update 6 files

--- src/constants/links.constants.ts
+++ src/constants/links.constants.ts
@@ -2,5 +2,5 @@ export const LOGO = "/";
 export const HIRE = "https://usemotion.com/meet/ygt5wsm/meeting?d=30";
 export const LINKED = "https://www.linkedin.com/in/dktv/";
 export const TG = "https://t.me/Joichiro";
-export const ARTICLES = "https://paragraph.xyz/@ketov.eth";
+export const ARTICLES = "https://usemotion.com/meet/ygt5wsm/meeting?d=30";
 export const EMAIL = "danielketovone@gmail.com";
